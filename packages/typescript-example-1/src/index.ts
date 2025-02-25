8144a09dd2ba28dc33781c853431ee2e847208ad
Keith E <129207802+kiloecho-sonar@users.noreply.github.com>
2025-02-25 13:25:55 -0600

    Update index.ts
    Adding in issue X ≠ Y

--- packages/typescript-example-1/src/index.ts
+++ packages/typescript-example-1/src/index.ts
@@ -4,5 +4,10 @@ import { world } from "@mme/typescript-example-2";
  * Hello World
  */
 export function helloWorld() {
+  let x = 0;
+  let y = 1;
+  if (x != y) {
+    console.log("X > Y");
+  }
   return `Hello ${world()}`;
 }
