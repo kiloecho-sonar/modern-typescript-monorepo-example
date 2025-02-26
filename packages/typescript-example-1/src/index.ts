cc8a31807406ad4be4c6e65572c20030948bd32f
Keith E <129207802+kiloecho-sonar@users.noreply.github.com>
2025-02-25 22:00:13 -0600

    Update index.ts
    Removed garbage for testing

--- packages/typescript-example-1/src/index.ts
+++ packages/typescript-example-1/src/index.ts
@@ -4,11 +4,5 @@ import { world } from "@mme/typescript-example-2";
  * Hello World
  */
 export function helloWorld() {
-  let x = 0;
-  let y = 1;
-  if (x == y) {
-    console.log("X > Y");
-    return 'Hello Sucka!';
-  }
   return `Hello ${world()}`;
 }
